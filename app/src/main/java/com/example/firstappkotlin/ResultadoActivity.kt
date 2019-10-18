48b4b95fe3554e2a9a98c4eafa86d66837d35776
Amanda Rodrigues <amanda.nrodrigues@hotmail.com>
2019-10-17 20:01:10 -0400

    Melhoria no visual da aplicação

--- app/src/main/java/com/example/firstappkotlin/ResultadoActivity.kt
+++ app/src/main/java/com/example/firstappkotlin/ResultadoActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_resultado.*
-
+import kotlin.random.Random
 
 
 class ResultadoActivity : AppCompatActivity() {
@@ -23,43 +23,128 @@ class ResultadoActivity : AppCompatActivity() {
         var n3: Int = intent.getIntExtra("n3",0)
 
         if(numero==0 && n2==0 && n3==0){
-            img1.setImageResource(R.drawable.moeda_cara)
-            img2.setImageResource(R.drawable.moeda_cara)
-            img3.setImageResource(R.drawable.moeda_cara)
+            img1.setImageResource(R.drawable.moeda_coroa)
+            img2.setImageResource(R.drawable.moeda_coroa)
+            img3.setImageResource(R.drawable.moeda_coroa)
+            tV1.setText("Coroa")
+            tv2.setText("Coroa")
+            tv3.setText("Coroa")
         }else if(numero==1 && n2==1 && n3==1) {
-            img1.setImageResource(R.drawable.moeda_coroa)
-            img2.setImageResource(R.drawable.moeda_coroa)
-            img3.setImageResource(R.drawable.moeda_coroa)
+            img1.setImageResource(R.drawable.moeda_cara)
+            img2.setImageResource(R.drawable.moeda_cara)
+            img3.setImageResource(R.drawable.moeda_cara)
+            tV1.setText("Cara")
+            tv2.setText("Cara")
+            tv3.setText("Cara")
         }else if(numero==0 && n2==1 && n3==1) {
-            img1.setImageResource(R.drawable.moeda_cara)
-            img2.setImageResource(R.drawable.moeda_coroa)
-            img3.setImageResource(R.drawable.moeda_coroa)
+            img1.setImageResource(R.drawable.moeda_coroa)
+            img2.setImageResource(R.drawable.moeda_cara)
+            img3.setImageResource(R.drawable.moeda_cara)
+            tV1.setText("Coroa")
+            tv2.setText("Cara")
+            tv3.setText("Cara")
         }else if(numero==0 && n2==0 && n3==1) {
-            img1.setImageResource(R.drawable.moeda_cara)
-            img2.setImageResource(R.drawable.moeda_cara)
-            img3.setImageResource(R.drawable.moeda_coroa)
+            img1.setImageResource(R.drawable.moeda_coroa)
+            img2.setImageResource(R.drawable.moeda_coroa)
+            img3.setImageResource(R.drawable.moeda_cara)
+            tV1.setText("Coroa")
+            tv2.setText("Coroa")
+            tv3.setText("Cara")
         }else if(numero==1 && n2==1&& n3==0) {
-            img1.setImageResource(R.drawable.moeda_coroa)
-            img2.setImageResource(R.drawable.moeda_coroa)
-            img3.setImageResource(R.drawable.moeda_cara)
-        }else if(numero==1 && n2==0 && n3==0) {
-            img1.setImageResource(R.drawable.moeda_coroa)
-            img2.setImageResource(R.drawable.moeda_cara)
-            img3.setImageResource(R.drawable.moeda_cara)
-        }else if(numero==1 && n2==0 && n3==1) {
-            img1.setImageResource(R.drawable.moeda_coroa)
+            img1.setImageResource(R.drawable.moeda_cara)
             img2.setImageResource(R.drawable.moeda_cara)
             img3.setImageResource(R.drawable.moeda_coroa)
-        }else if(numero==0 && n2==1 && n3==0) {
+            tV1.setText("Cara")
+            tv2.setText("Cara")
+            tv3.setText("Coroa")
+        }else if(numero==1 && n2==0 && n3==0) {
+            img1.setImageResource(R.drawable.moeda_cara)
+            img2.setImageResource(R.drawable.moeda_coroa)
+            img3.setImageResource(R.drawable.moeda_coroa)
+            tV1.setText("Cara")
+            tv2.setText("Coroa")
+            tv3.setText("Coroa")
+        }else if(numero==1 && n2==0 && n3==1) {
             img1.setImageResource(R.drawable.moeda_cara)
             img2.setImageResource(R.drawable.moeda_coroa)
             img3.setImageResource(R.drawable.moeda_cara)
+            tV1.setText("Cara")
+            tv2.setText("Coroa")
+            tv3.setText("Cara")
+        }else if(numero==0 && n2==1 && n3==0) {
+            img1.setImageResource(R.drawable.moeda_coroa)
+            img2.setImageResource(R.drawable.moeda_cara)
+            img3.setImageResource(R.drawable.moeda_coroa)
+            tV1.setText("Coroa")
+            tv2.setText("Cara")
+            tv3.setText("Coroa")
         }
 
 
-        button_voltar.setOnClickListener{
-            finish()
-        }
+        play.setOnClickListener{
+            var numero = Random.nextInt(2) //gerando um nuemro randomico até 1 ( ou seja, 0 ou 1)
+            var n2 = Random.nextInt(2)
+            var n3 = Random.nextInt(2)
 
+            if(numero==0 && n2==0 && n3==0){
+                img1.setImageResource(R.drawable.moeda_coroa)
+                img2.setImageResource(R.drawable.moeda_coroa)
+                img3.setImageResource(R.drawable.moeda_coroa)
+                tV1.setText("Coroa")
+                tv2.setText("Coroa")
+                tv3.setText("Coroa")
+            }else if(numero==1 && n2==1 && n3==1) {
+                img1.setImageResource(R.drawable.moeda_cara)
+                img2.setImageResource(R.drawable.moeda_cara)
+                img3.setImageResource(R.drawable.moeda_cara)
+                tV1.setText("Cara")
+                tv2.setText("Cara")
+                tv3.setText("Cara")
+            }else if(numero==0 && n2==1 && n3==1) {
+                img1.setImageResource(R.drawable.moeda_coroa)
+                img2.setImageResource(R.drawable.moeda_cara)
+                img3.setImageResource(R.drawable.moeda_cara)
+                tV1.setText("Coroa")
+                tv2.setText("Cara")
+                tv3.setText("Cara")
+            }else if(numero==0 && n2==0 && n3==1) {
+                img1.setImageResource(R.drawable.moeda_coroa)
+                img2.setImageResource(R.drawable.moeda_coroa)
+                img3.setImageResource(R.drawable.moeda_cara)
+                tV1.setText("Coroa")
+                tv2.setText("Coroa")
+                tv3.setText("Cara")
+            }else if(numero==1 && n2==1&& n3==0) {
+                img1.setImageResource(R.drawable.moeda_cara)
+                img2.setImageResource(R.drawable.moeda_cara)
+                img3.setImageResource(R.drawable.moeda_coroa)
+                tV1.setText("Cara")
+                tv2.setText("Cara")
+                tv3.setText("Coroa")
+            }else if(numero==1 && n2==0 && n3==0) {
+                img1.setImageResource(R.drawable.moeda_cara)
+                img2.setImageResource(R.drawable.moeda_coroa)
+                img3.setImageResource(R.drawable.moeda_coroa)
+                tV1.setText("Cara")
+                tv2.setText("Coroa")
+                tv3.setText("Coroa")
+            }else if(numero==1 && n2==0 && n3==1) {
+                img1.setImageResource(R.drawable.moeda_cara)
+                img2.setImageResource(R.drawable.moeda_coroa)
+                img3.setImageResource(R.drawable.moeda_cara)
+                tV1.setText("Cara")
+                tv2.setText("Coroa")
+                tv3.setText("Cara")
+            }else if(numero==0 && n2==1 && n3==0) {
+                img1.setImageResource(R.drawable.moeda_coroa)
+                img2.setImageResource(R.drawable.moeda_cara)
+                img3.setImageResource(R.drawable.moeda_coroa)
+                tV1.setText("Coroa")
+                tv2.setText("Cara")
+                tv3.setText("Coroa")
+            }
+
+
+        }
     }
 }
